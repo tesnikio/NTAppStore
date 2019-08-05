c64a606ee8586775ee4d0c1d75cf637b8950b544
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-08-05 16:24:01 +0300

    initial commit

---
+++ NTAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NTAppStore
+//
+//  Created by Nikita Teslyuk on 05/08/2019.
+//  Copyright © 2019 Tesnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
